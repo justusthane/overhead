9f86d4cfe76b5920ad6c5a9b428c3ced504d2623
Justus Grunow <jg@justus.ws>
2021-08-28 13:41:18 -0400

    Now outputs results to index.html

--- overhead.py
+++ overhead.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from datetime import datetime
+from string import Template
 
 url = "https://data-live.flightradar24.com/zones/fcgi/feed.js?faa=1&bounds=48.395%2C48.382%2C-89.279%2C-89.229&satellite=1&mlat=1&flarm=1&adsb=1&gnd=1&air=1&vehicles=1&estimated=1&maxage=14400&gliders=1&stats=1"
 headers = {
@@ -15,21 +16,50 @@ headers = {
     'Sec-Fetch-Site': 'same-site',
     'TE': 'trailers'
 }
+
+templateString = """
+<html>
+<head>
+<title>Overhead</title>
+</head>
+<body>
+<ul>
+<li>Registration: ${reg}
+<li>Departure airport: ${dptAirport}
+<li>Arrival airport: ${arrAirport}
+<li>Altiude: ${altitude}
+<li>Type: ${type}
+</body>
+</html>
+"""
+
+
+template = Template(templateString)
+
 response = requests.get(url, headers=headers)
 responseJson = response.json()
 if responseJson['stats']['visible']['ads-b'] == 0:
-    f = open("planelog.txt","a")
-    f.write(datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
-    f.write("\n")
-    f.write("No planes in the sky" + "\n\n")
-    f.close()
+    print("No planes in the sky" + "\n")
 else:
     for key in responseJson:
         if key != "full_count" and key != "version" and key != "stats":
-            f = open("planelog.txt","a")
+            f = open("/root/planelog.txt","a")
             f.write(datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + "\n")
             f.write("Plane! Plane! Plane!\n")
             f.write("Key: " + key + "\n")
-            f.write(responseJson[key])
+            #f.write(json.dumps(responseJson[key]))
+            for i in range(len(responseJson[key])):
+                    f.write(str(i) + ": " + str(responseJson[key][i]) + "\n")
             f.write("\n\n")
             f.close()
+            planeDict = {
+                'reg': responseJson[key][9],
+                'dptAirport': responseJson[key][11],
+                'arrAirport': responseJson[key][12],
+                'altitude': responseJson[key][4],
+                'type': responseJson[key][8]
+                }
+            f = open("/var/www/overhead/index.html","w")
+            f.write(template.safe_substitute(planeDict))
+            f.close()
+
